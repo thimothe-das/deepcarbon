ce0b3ec823eec81bfff313b8f8fef39822f41e63
Lucas Lefort <negames64300@gmail.com>
2019-12-17 19:32:56 +0100

    mise en place assets WIP

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -9,42 +9,36 @@ class LandingController extends AbstractController
     public function index()
     {
         return $this->render('landing/index.html.twig', [
-            'controller_name' => 'LandingController',
         ]);
     }
 
     public function about()
     {
         return $this->render('landing/about.html.twig', [
-            'controller_name' => 'LandingController',
         ]);
     }
 
     public function domain()
     {
         return $this->render('landing/domain.html.twig', [
-            'controller_name' => 'LandingController',
         ]);
     }
 
     public function hosting()
     {
         return $this->render('landing/hosting.html.twig', [
-            'controller_name' => 'LandingController',
         ]);
     }
 
     public function blog()
     {
         return $this->render('landing/blog.html.twig', [
-            'controller_name' => 'LandingController',
         ]);
     }
 
     public function contact()
     {
         return $this->render('landing/contact.html.twig', [
-            'controller_name' => 'LandingController',
         ]);
     }
 }
